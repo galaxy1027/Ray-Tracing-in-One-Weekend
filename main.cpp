8d8d6cf4e449b61ef360f9d5f6c34af9dd9fef66
Branford Coleman <77608551+galaxy1027@users.noreply.github.com>
2023-09-16 23:59:16 -0700

    Implemented ray tracing method. Sphere class created to add spheres to scene. Camera moved to seperate class.

--- main.cpp
+++ main.cpp
@@ -1,72 +1,19 @@
-#include "vec3.hpp"
-#include "color.hpp"
-#include "ray.h"
+#include "rtweekend.hpp"
 
-#include <iostream>
-
-
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
-}
-
-
-color ray_color(const ray& r) {
-
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
+#include "camera.hpp"
+#include "hittable_list.hpp"
+#include "sphere.hpp"
 
 int main() {
+    hittable_list world;
 
-    //Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
+    camera cam;
 
-    //Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
-    auto camera_center = point3(0, 0, 0);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = 400;
 
-    //Calculate vectors across horizontal and down vertical viewport edges
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
-
-    //Calculate delta vectors from pixel to pixel
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    //Calculate location of upper left pixel
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-
-    //Renderer
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; ++j) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
-        }
-    }
-    std::clog << "\rDone.                       \n";
+    cam.render(world);
 }
---
+++ rtweekend.hpp
@@ -0,0 +1,29 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <climits>
+#include <memory>
+
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+//Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+//Utility functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180;
+}
+
+
+//Common headers
+#include "interval.hpp"
+#include "ray.hpp"
+#include "vec3.hpp"
+
+#endif
