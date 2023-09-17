d54999ed176bb89b389b6297bbe9b5fa3e49ffff
galaxy1027 <branford2001@gmail.com>
2023-09-17 01:53:17 -0700

    Implemented material class for diffuse/metal materials

--- camera.hpp
+++ camera.hpp
@@ -4,6 +4,7 @@
 #include "rtweekend.hpp"
 #include "color.hpp"
 #include "hittable.hpp"
+#include "material.hpp"
 #include <iostream>
 
 class camera {
@@ -90,8 +91,12 @@ private:
         }
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.1 * ray_color(ray(rec.p, direction), depth - 1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                return attenuation * ray_color(scattered, depth - 1, world);
+            }
+            return color(0, 0, 0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
